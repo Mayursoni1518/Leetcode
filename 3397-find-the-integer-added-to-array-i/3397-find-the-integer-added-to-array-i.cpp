9e09c47aa625b5785fdaf0a494ef2a9866c0153d
Mayur <147459454+Mayursoni1518@users.noreply.github.com>
2024-11-01 17:32:17 +0530

    Time: 0 ms (100%), Space: 34.8 MB (48.46%) - LeetHub

--- 3397-find-the-integer-added-to-array-i/3397-find-the-integer-added-to-array-i.cpp
+++ 3397-find-the-integer-added-to-array-i/3397-find-the-integer-added-to-array-i.cpp
@@ -8,3 +8,19 @@ public:
         return nums2[n-1] - nums1[n-1] ;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int addedInteger(vector<int>& nums1, vector<int>& nums2) {
+//         int max1=-1,max2=-1;
+//         for(int i=0;i<nums1.size();i++){
+//             max1=max(nums1[i],max1);
+//             max2=max(nums2[i],max2);
+//         }
+
+//         return max2-max1;
+
+//     }
+// };
