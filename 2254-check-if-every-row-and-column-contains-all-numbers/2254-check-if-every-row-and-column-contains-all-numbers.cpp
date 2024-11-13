75e0f3d50e495132ff50e8b82c32c57d9a40511d
Mayur <147459454+Mayursoni1518@users.noreply.github.com>
2024-11-13 08:12:45 +0530

    Time: 162 ms (36.31%), Space: 96.2 MB (28.99%) - LeetHub

---
+++ 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkValid(vector<vector<int>>& matrix) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        for (int i = 0; i < row; i++) {
+            unordered_set<int>temp;
+            for (int j = 0; j < col; j++) {
+                temp.insert(matrix[i][j]);
+            }
+            if (temp.size() != row) {
+                return false;
+            }
+        }
+        for (int i = 0; i < col; i++) {
+            unordered_set<int> temp;
+            for (int j = 0; j < row; j++) {
+                temp.insert(matrix[j][i]);
+            }
+            if (temp.size() != row) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
