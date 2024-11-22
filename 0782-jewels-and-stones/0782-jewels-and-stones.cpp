6b4e874e0bdc40c5aa3d8ba1d06650c4da5d43ec
Mayur <147459454+Mayursoni1518@users.noreply.github.com>
2024-11-22 23:06:55 +0530

    Time: 0 ms (100%), Space: 8 MB (84.02%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.cpp
+++ 0782-jewels-and-stones/0782-jewels-and-stones.cpp
@@ -12,3 +12,11 @@ public:
         return count;
     }
 };
+
+    // int numJewelsInStones(string J, string S) {
+    //     int res = 0;
+    //     unordered_set<char> setJ(J.begin(), J.end());
+    //     for (char s : S)
+    //         if (setJ.count(s)) res++;
+    //     return res;
+    // }
