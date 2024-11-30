f831141edb0c62be60c8e3368e013aedca2ee624
Mayur <147459454+Mayursoni1518@users.noreply.github.com>
2024-11-30 12:37:04 +0530

    Update 0011-container-with-most-water.cpp

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -6,7 +6,7 @@ public:
         int maxArea = 0;
 
         while (left < right) {
-            int currentArea = min(height[left], height[right]) * (right - left);
+            int currentArea = min(height[left], height[right]) * (right - left));
             maxArea = max(maxArea, currentArea);
 
             if (height[left] < height[right]) {
