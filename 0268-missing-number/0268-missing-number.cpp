88c8349cfbea05364e1e31be0bdfe1fe3f3bfc9e
Mayur <147459454+Mayursoni1518@users.noreply.github.com>
2025-01-29 13:02:57 +0530

    Time: 0 ms (100%), Space: 21.8 MB (70.86%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,34 +1,12 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin() , nums.end()) ;
-        int ans ; 
-        for(int i = 0 ;  i < nums.size() ; i++){
-            if(nums[i] == i ) {
-                continue ;
-            }
-            else{
-                ans = i  ;
-                return ans; 
+        sort(nums.begin() , nums.end()) ; 
+        for(int i = 0 ; i < nums.size() ; i++  ){
+            if(nums[i] != i ){
+                return i ; 
             }
         }
         return nums.size() ; 
-        
     }
 };
-
-// class Solution {
-// public:
-//     int missingNumber(vector<int>& nums) {
-//         int n = nums.size();
-//         int ans =0;
-//         for(int i =1;i<=n;i++){
-//             ans = ans ^ i;
-//         }
-//         for(int i =0;i<nums.size();i++){
-//             ans= ans^nums[i];
-//         }
-//         return ans;
-//     }
-// };
-
