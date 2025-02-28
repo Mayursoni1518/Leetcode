a3363d4ab562591bd10ede66e08cb66ada905a53
Mayur <147459454+Mayursoni1518@users.noreply.github.com>
2025-02-28 14:00:38 +0530

    Time: 127 ms (67.16%), Space: 10.1 MB (67.35%) - LeetHub

--- 1331-path-with-maximum-gold/1331-path-with-maximum-gold.cpp
+++ 1331-path-with-maximum-gold/1331-path-with-maximum-gold.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
-    vector<int> roww = {1, -1, 0, 0};
-    vector<int> coll = {0, 0, -1, 1};
+    vector<vector<int>> directions{{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
     int maxGold = 0;
-
-    int dfs(vector<vector<int>>& grid, int x, int y, int n, int m) {
-        if (x < 0 || x >= n || y < 0 || y >= m || grid[x][y] == 0) return 0;
-        
-        int curr = grid[x][y];
-        grid[x][y] = 0;
-        int localMaxGold = curr;
-
-        for (int i = 0; i < 4; i++) {
-            int newX = x + roww[i];
-            int newY = y + coll[i];
-            localMaxGold = max(localMaxGold, curr + dfs(grid, newX, newY, n, m));
+    int DFS(vector<vector<int>>& grid, int i, int j, int m, int n) {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) {
+            return 0;
         }
+        int currentGold = grid[i][j];
+        grid[i][j] = 0;
+        int maxGold = 0;
+        for (auto& dir : directions) {
+            int new_i = i + dir[0];
+            int new_j = j + dir[1];
 
-        grid[x][y] = curr;
-        return localMaxGold;
+            maxGold = max(maxGold, DFS(grid, new_i, new_j, m, n));
+        }
+        grid[i][j] = currentGold;
+        return maxGold + currentGold ;
     }
-
     int getMaximumGold(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (grid[i][j] != 0) {
-                    maxGold = max(maxGold, dfs(grid, i, j, n, m));
+                    maxGold = max(maxGold, DFS(grid, i, j, m, n));
                 }
             }
         }
-
         return maxGold;
     }
 };
