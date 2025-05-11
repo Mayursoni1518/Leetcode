2e95fb05da3592004c311b0a521d9cff213bcfdf
Mayur <147459454+Mayursoni1518@users.noreply.github.com>
2025-05-11 16:29:56 +0530

    Time: 0 ms (100%), Space: 50.2 MB (29.3%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-        int n = nums.size();
-        int cnt = 0;
-        int maxi = -1;
-        int i = 0;
-        int j = 0;
-
-        while (j < n) {
-            if (nums[j] == 0) {
-                i = j + 1 ; 
+        int n = nums.size() ;
+        int cnt = 0 ;
+        int i = 0 ;
+        int j = 0 ;
+        int maxi = 0 ; 
+        while(j < n )
+        {
+            if(nums[j] == 1 )
+            {
+                cnt++ ;
+                maxi = max(maxi , cnt ) ; 
             }
-            maxi = max(maxi, j - i + 1);
-            j++;
-        }
+            else{
+                cnt = 0 ; 
+            }
+            j++ ; 
+        } 
         return maxi ; 
     }
 };
