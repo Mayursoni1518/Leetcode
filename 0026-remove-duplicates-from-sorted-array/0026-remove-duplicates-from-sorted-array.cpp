e59b5b4e71c2b437b1ed63d01b8213f9c5049110
Mayur <147459454+Mayursoni1518@users.noreply.github.com>
2025-06-17 23:54:31 +0530

    Time: 3 ms (15.98%), Space: 22.6 MB (78.47%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-       int j = 1 ; 
-
-       for(int i = 1 ; i < nums.size() ; i++){
-        if(nums[i] != nums[i-1]){
-            nums[j] = nums[i] ;
-            j++;
+       int i = 0 ; 
+       int cnt = 0 ;
+       for(int j = 1 ; j < nums.size() ; j++ )
+       {
+        if(nums[j] != nums[i]) {
+            cnt++ ;
+            i++ ; 
+            nums[i] = nums[j] ; 
         }
        }
-       return j; 
+       return cnt + 1 ; 
     }
 };
-
-// It say's that you have to return the size of the compressed array with no duplicates , while you can see the vector is call by reference so it checks whether the array (nums) you have updated or not , while ignoring after the required spaces
