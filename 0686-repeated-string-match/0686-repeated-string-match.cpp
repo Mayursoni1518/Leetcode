65fa001656a372545de77889abc5535f7fe47dee
Mayur <147459454+Mayursoni1518@users.noreply.github.com>
2025-08-03 14:26:46 +0530

    Time: 4 ms (60.73%), Space: 9.6 MB (93.1%) - LeetHub

---
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int repeatedStringMatch(string a, string b) {
+        string repeat = a; 
+        int count = 1 ;
+
+        while(repeat.size() < b.size()) {
+            repeat += a ;
+            count++ ; 
+        }
+        if(repeat.find(b) != string::npos) {
+            return count ; 
+        }
+        repeat += a ;
+        count++ ;
+        if(repeat.find(b) != string::npos) {
+            return count ; 
+        }
+        return -1 ; 
+    }
+};
