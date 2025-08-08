1fea4338b22e8592167fff45859d89fbba363387
Mayur <147459454+Mayursoni1518@users.noreply.github.com>
2025-08-08 09:50:56 +0530

    Time: 0 ms (100%), Space: 15 MB (24.75%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
+    int value(ListNode* head ) {
+        int cnt = 0 ;
+        while(head != nullptr ) {
+            cnt++ ;
+            head = head -> next ; 
+        }
+        return cnt ; 
+    }
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        // Step 1: Calculate the size of the linked list
-        ListNode* temp = head;
-        int size = 0;
-        while (temp != nullptr) {
-            temp = temp->next;
-            size++;
+        ListNode* dummy = new ListNode(-1) ;
+        dummy->next = head ; 
+        ListNode* temp = dummy ; 
+        int pos = value(head) - n ; 
+
+        for(int i = 0 ; i< pos ; i++ ) {
+            temp = temp -> next ; 
         }
-
-        // Step 2: Handle edge case: removing the head node
-        if (n == size) {
-            return head->next; // Remove the head and return the new head
-        }
-
-        // Step 3: Traverse to the node just before the node to be removed
-        temp = head; // Reset temp to the start of the list
-        int stepsToTake = size - n - 1; // Stop one node before the target
-        for (int i = 0; i < stepsToTake; i++) {
-            temp = temp->next;
-        }
-
-        // Step 4: Remove the target node
-        temp->next = temp->next->next;
-
-        return head;
+        temp -> next = temp->next->next ;
+        return dummy->next; 
     }
 };
