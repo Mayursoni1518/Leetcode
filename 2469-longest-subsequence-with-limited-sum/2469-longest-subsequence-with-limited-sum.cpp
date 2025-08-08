2b9d8ced3a20da4503bfb631a95915e9deaed17e
Mayur <147459454+Mayursoni1518@users.noreply.github.com>
2025-08-08 14:50:50 +0530

    Time: 11 ms (21.7%), Space: 18.1 MB (20.7%) - LeetHub

--- 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.cpp
+++ 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.cpp
@@ -2,32 +2,26 @@ class Solution {
 public:
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
         sort(nums.begin(), nums.end());
-        int sum = 0;
-        vector<int>Prefixans ;
-        int n = nums.size();
-         for (int i = 0; i < nums.size(); i++) {
-            sum += nums[i];
-            Prefixans.push_back(sum);
+        vector<int> prefix(nums.size());
+        prefix[0] = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            prefix[i] = prefix[i - 1] + nums[i];
         }
-
-        vector<int> result;
-        for (auto& query : queries) {
-            int left = 0;
-            int right = n - 1;
-            int ans = 0;
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-                if (Prefixans[mid] <= query) {
-                    ans = (mid + 1);
-                    left++;
-                } else {
-                    right--;
+        int j = 0;
+        vector<int> ans;
+        int n = queries.size();
+        while (j < n) {
+            for (int i = 0; i < nums.size(); i++) {
+                if (prefix[i] > queries[j]) {
+                    ans.push_back(i);
+                    break;
                 }
-               
-               
+                if (i == nums.size() - 1) {
+                    ans.push_back(nums.size());
+                }
             }
-            result.push_back(ans);
+            j++;
         }
-        return result;
+        return ans;
     }
 };
