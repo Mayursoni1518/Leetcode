fba6370b67ce2d5d2b0413f2faf91970076cba14
Mayur <147459454+Mayursoni1518@users.noreply.github.com>
2025-08-08 16:43:39 +0530

    Time: 47 ms (41.7%), Space: 128.4 MB (27.84%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
-        int m = potions.size();
-        sort(potions.begin(), potions.end());  // Sort for binary search
-        vector<int> result;
-
-        for (int spell : spells) {
-            int left = 0, right = m - 1, idx = m;
-
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-                long long product = 1LL * spell * potions[mid];
-
-                if (product >= success) {
-                    idx = mid;
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions,
+                                long long success) {
+        vector<int> ans;
+        sort(potions.begin(), potions.end());
+        int n = potions.size();
+        for (int i = 0; i < spells.size(); i++) {
+            int start = 0;
+            int end = potions.size() - 1;
+            int res = n;
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+               if((long long) spells[i] * potions[mid] >= success) {
+                res = mid ;
+                end = mid - 1 ; 
+               }
+               else{
+                start = mid + 1 ; 
+               }
             }
-
-            result.push_back(m - idx);
+            ans.push_back(n - res);
         }
-
-        return result;
+        return ans;
     }
 };
