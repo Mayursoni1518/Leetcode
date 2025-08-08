010c091f0080def5c1575a02a231f6da3170f10e
Mayur <147459454+Mayursoni1518@users.noreply.github.com>
2025-08-08 15:05:08 +0530

    Time: 6 ms (40.64%), Space: 18.3 MB (13.03%) - LeetHub

--- 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.cpp
+++ 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.cpp
@@ -2,26 +2,65 @@ class Solution {
 public:
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
         sort(nums.begin(), nums.end());
+        
+        // Build prefix sum
         vector<int> prefix(nums.size());
         prefix[0] = nums[0];
         for (int i = 1; i < nums.size(); i++) {
             prefix[i] = prefix[i - 1] + nums[i];
         }
-        int j = 0;
-        vector<int> ans;
-        int n = queries.size();
-        while (j < n) {
-            for (int i = 0; i < nums.size(); i++) {
-                if (prefix[i] > queries[j]) {
-                    ans.push_back(i);
-                    break;
-                }
-                if (i == nums.size() - 1) {
-                    ans.push_back(nums.size());
+
+        vector<int> res;
+
+        for (int q = 0; q < queries.size(); q++) {
+            int target = queries[q];
+            int left = 0, right = nums.size() - 1;
+            int ans = 0;
+
+            // Binary Search for upper_bound (first prefix[i] > target)
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+
+                if (prefix[mid] <= target) {
+                    ans = mid + 1;  // valid subarray size
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
                 }
             }
-            j++;
+
+            res.push_back(ans);
         }
-        return ans;
+
+        return res;
     }
 };
+
+// in o(n2) 
+// class Solution {
+// public:
+//     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+//         sort(nums.begin(), nums.end());
+//         vector<int> prefix(nums.size());
+//         prefix[0] = nums[0];
+//         for (int i = 1; i < nums.size(); i++) {
+//             prefix[i] = prefix[i - 1] + nums[i];
+//         }
+//         int j = 0;
+//         vector<int> ans;
+//         int n = queries.size();
+//         while (j < n) {
+//             for (int i = 0; i < nums.size(); i++) {
+//                 if (prefix[i] > queries[j]) {
+//                     ans.push_back(i);
+//                     break;
+//                 }
+//                 if (i == nums.size() - 1) {
+//                     ans.push_back(nums.size());
+//                 }
+//             }
+//             j++;
+//         }
+//         return ans;
+//     }
+// };
