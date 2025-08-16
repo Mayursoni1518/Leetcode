94344dc37ec6288b3736bc153c406504229c960a
Mayur <147459454+Mayursoni1518@users.noreply.github.com>
2025-08-16 22:21:10 +0530

    Time: 0 ms (100%), Space: 8.1 MB (41.82%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.cpp
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-    int maximum69Number(int num) {
-        vector<int> ans;
-        while (num != 0) {
-            int temp = num % 10;
-            ans.push_back(temp);
-            num /= 10;
-        }
+    int maximum69Number (int num) {
+        string s = to_string(num) ;
 
-        reverse(ans.begin(), ans.end());
-        for (int i = 0; i < ans.size(); i++) {
-            if (ans[i] == 6) {
-                ans[i] = 9;
-                break;
+        for(char i = 0 ; i <s.size() ; i++){
+            if(s[i] == '6'){
+                s[i] = '9' ; 
+                break ;
             }
         }
-        int result = 0;
-        for (int i = 0; i < ans.size(); i++) {
-            result = result * 10 + ans[i];
-        }
-        return result;
+        return stoi(s) ;
     }
 };
