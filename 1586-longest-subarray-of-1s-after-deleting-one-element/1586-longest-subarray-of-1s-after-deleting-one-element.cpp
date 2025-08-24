fb6b4f7466b0e1704353bc8b2e1f2c76356fd7d7
Mayur <147459454+Mayursoni1518@users.noreply.github.com>
2025-08-24 09:19:17 +0530

    Time: 0 ms (100%), Space: 60 MB (59.08%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,99 +1,23 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-
-        int zeroCount = 0;
-        int longestWindow = 0;
-
-        int i = 0;
-        
-        for (int j = 0; j < nums.size(); j++) {
-            zeroCount += (nums[j] == 0);
-                          
-            // Shrink the window until the zero counts come under the limit.
-            while (zeroCount > 1) {
-                zeroCount -= (nums[i] == 0);
-                i++;
+        int n = nums.size();
+        int i = 0 ;
+        long cnt1 = 0 ;
+        long cnt0 = 0 ;
+        int maxi = INT_MIN;
+        int j = 0 ;
+        while(j < n ) {
+            if(nums[j] == 1) { cnt1++ ; } 
+            if(nums[j] == 0 ) { cnt0++ ; } 
+            while(cnt0 > 1 ) {
+                if(nums[i] == 0 ) { cnt0-- ; }
+                if(nums[i] == 1 ) { cnt1++ ; }
+                i++ ;
             }
-              
-            longestWindow = max(longestWindow, j - i);
+            maxi = max(maxi , j - i + 1 ) ; 
+            j++;
         }
-
-        return longestWindow;
+        return maxi - 1 ; 
     }
 };
-
-// this is all approaches => 
-//Approach-1 (Simple Brute Force) - T.C : O(n^2)
-// class Solution {
-// public:
-//     int n;
-    
-//     int findMax(vector<int>& nums, int skip_idx) {
-        
-//         int currLength = 0;
-//         int maxLength  = 0;
-        
-//         for(int i = 0; i<n; i++) {
-//             if(i == skip_idx)
-//                 continue;
-            
-//             if(nums[i] == 1) {
-//                 currLength++;
-//                 maxLength = max(maxLength, currLength);
-//             } else {
-//                 currLength = 0;
-//             }
-//         }
-        
-//         return maxLength;
-        
-//     }
-    
-//     int longestSubarray(vector<int>& nums) {
-//         n = nums.size();
-//         int result = 0;
-        
-//         int countZero = 0;
-//         for(int i = 0; i<n; i++) {
-            
-//             if(nums[i] == 0) {
-//                 countZero++;
-//                 result = max(result, findMax(nums, i));
-//             }
-            
-//         }
-        
-//         if(countZero == 0)
-//             return n-1;
-        
-//         return result;
-        
-//     }
-// };
-
-
-//Approach-3 (Using Better Sliding Window) - O(n))
-// class Solution {
-// public:
-//     int longestSubarray(vector<int>& nums) {
-//         int i = 0, j = 0;
-        
-//         int last_zero_idx = -1;
-        
-//         int result = 0;
-        
-//         while(j < nums.size()) {
-            
-//             if(nums[j] == 0) {
-//                 i = last_zero_idx+1;
-//                 last_zero_idx = j;
-//             }
-            
-//             result = max(result, j-i);
-//             j++;
-//         }
-        
-//         return result;
-//     }
-// };
