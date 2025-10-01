26cff110c955b83d3e7b2d8341e90e925a2caa33
Mayur <147459454+Mayursoni1518@users.noreply.github.com>
2025-10-01 18:25:43 +0530

    Time: 838 ms (5.88%), Space: 17.6 MB (26.82%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        sort(nums.begin() , nums.end()) ;
-        vector<vector<int>> result;
-        for (int i = 0; i < n - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
+        vector<vector<int>>ans ; 
+        sort(nums.begin() , nums.end()) ; 
+        int n = nums.size() ;
+        for(int i = 0 ; i < n - 3 ; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) {
+                continue ; 
             }
-            for (int j = i + 1; j < n - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) {
-                    continue;
+            for(int j = i + 1 ; j < n -2 ; j++) {
+                if( j > i  + 1 && nums[j] == nums[j-1]){
+                    continue ; 
                 }
-                for (int k = j + 1; k < n - 1; k++) {
-                    if (k > j + 1 && nums[k] == nums[k - 1]) {
-                        continue;
+                for(int k = j + 1 ; k < n - 1 ; k++) {
+                    if(k > j + 1  && nums[k] == nums[k-1]) {
+                        continue ; 
                     }
-                    for (int r = k + 1; r < n; r++) {
-                        if (r > k + 1 && nums[r] == nums[r - 1]) {
-                            continue;
+                    for(int z = k + 1 ; z < n ; z++) {
+                        if( z > k + 1 && nums[z] == nums[z-1]) {
+                            continue ; 
                         }
-                        long value = (long)nums[i] + nums[j] + nums[k] + nums[r]; 
-                        if (value == target) {
-                            vector<int> rr = {nums[i], nums[j], nums[k],nums[r]};
-                            result.push_back(rr) ; 
+                        long val = (long)nums[i] + nums[j] + nums[k] + nums[z] ;
+                        if(val == target) {
+                            vector<int>rr = {nums[i] , nums[j] , nums[k] , nums[z]} ; 
+                            ans.push_back(rr) ; 
                         }
                     }
                 }
             }
         }
-        return result;
+        return ans ; 
     }
 };
