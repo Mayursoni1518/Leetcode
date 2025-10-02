986c08ef135945aa0903d88ad2b1eaf01f559661
Mayur <147459454+Mayursoni1518@users.noreply.github.com>
2025-10-02 07:58:19 +0530

    Time: 0 ms (100%), Space: 13.2 MB (24.65%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -1,38 +1,23 @@
 class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
-        int n = nums.size() ; 
-      
-
-        for(int i = 0 ; i < nums.size()-1 ; i++ ){
+        int n = nums.size();
+        for(int i = 0 ; i < n - 1 ; i++ ) {
             if(nums[i] == nums[i+1]) {
-                nums[i] = nums[i] * 2 ;
+                nums[i] = (nums[i] * 2) ; 
                 nums[i+1] = 0 ; 
-            }
-            else{
-                continue; 
+                i++ ; 
             }
         }
-        int st = 0 ;
-        int end = st + 1 ;
-        while(st < n && end < n ){
-            if(nums[st] == 0 && nums[end] != 0 ){
-                swap(nums[st] , nums[end]) ;
-                st++ ;
-                end++ ; 
+        int i = 0 ;
+        int j = 0;
+        while(j < n ) {
+            if(nums[j] != 0 ) {
+                swap(nums[i] , nums[j]) ; 
+                i++ ;
             }
-            else if(nums[st] != 0 && nums[end] != 0 ){
-                st++ ;
-                end++ ;
-            }
-            else if(nums[st] != 0 && nums[end] == 0 ){
-                st++ ;
-                end++ ; 
-            }
-            else if(nums[st] == 0 && nums[end] == 0 ) {
-                end++ ; 
-            }
+            j++;
         }
-        return nums ; 
+        return nums; 
     }
 };
