afad2007af72c9421a1c2340c9215d86771606a3
Mayur <147459454+Mayursoni1518@users.noreply.github.com>
2025-10-03 08:06:36 +0530

    Time: 0 ms (100%), Space: 8.8 MB (6.21%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cpp
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
-    bool backspaceCompare(string s, string t) {
-        stack<char> st1, st2;
-        for (char c : s) {
-            if (c == '#') {
-                if (!st1.empty()) st1.pop();
-            } else {
-                st1.push(c);
+    bool backspaceCompare(string s, string t){
+        stack<char>st1, st2 ;
+        for(char c : s) {
+            if(c == '#') {
+                if(!st1.empty()) {
+                    st1.pop(); 
+                }
+            }
+            else{
+                st1.push(c) ; 
             }
         }
-        for (char c : t) {
-            if (c == '#') {
-                if (!st2.empty()) st2.pop();
-            } else {
-                st2.push(c);
+        for(char c : t) {
+            if(c == '#') {
+                if(!st2.empty()) {
+                    st2.pop(); 
+                }
+            }
+            else{
+                st2.push(c) ; 
             }
         }
-
-        while (!st1.empty() && !st2.empty()) {
-        if (st1.top() != st2.top()) {
-            return false;
+        string r1 , r2 ; 
+        while(!st1.empty()) {
+            r1 += st1.top();
+            st1.pop() ; 
         }
-        st1.pop();
-        st2.pop();
+        while(!st2.empty()) {
+            r2 += st2.top() ;
+            st2.pop() ;
+        }
+        return r1 == r2 ; 
     }
-    return st1.empty() && st2.empty();
-}
-
-        
 };
