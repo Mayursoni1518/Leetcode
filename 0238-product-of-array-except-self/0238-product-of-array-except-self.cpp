2c5f65fd9120c24c8ecca5f7ea873595492171ca
Mayur <147459454+Mayursoni1518@users.noreply.github.com>
2025-10-06 10:29:37 +0530

    Time: 0 ms (100%), Space: 40.1 MB (94.23%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        int prod = 1;
-        int prodZ =  1 ; 
-        vector<int>ans ; 
-        for (auto& i : nums) {
-            prod *= i;
-        }
-        for (auto& i : nums) {
-            if (i != 0) {
-                prodZ *= i;
-            }
-        }
-        int cnt = 0 ;
-        for(auto& i : nums) 
-        {
-            if(i == 0 ) {
+        int n= nums.size() ; 
+        int prod = 1 ;
+        int prodd = 1 ; 
+        int prodwithzero = 1 ;
+        vector<int>ans(n , 0 ); 
+        int cnt= 0; 
+        for(int i = 0 ; i <n; i++ ) {
+            if(nums[i] == 0 ) {
                 cnt++ ; 
             }
-        }
-        for (int i = 0; i < nums.size(); i++) {
-            if(prod == 0 ) {
-                if(nums[i] != 0 ) {
-                    ans.push_back(0) ; 
+            prod *= nums[i] ;
+            if(nums[i] != 0 ) {
+                prodwithzero *= nums[i] ; 
+            }
+            if(cnt > 1) {
+                return ans ; 
+            }
+        } 
+        for(int i = 0 ; i< n ; i++ ) {
+            if(cnt == 0 ) {
+                ans[i] = prodwithzero / nums[i] ; 
+            }
+            else if(cnt == 1) {
+                if(nums[i] == 0) {
+                    ans[i] = prodwithzero; 
                 }
                 else{
-                    if(cnt > 1 ){
-                        ans.push_back(0) ; 
-                    }
-                    else{
-                        ans.push_back(prodZ) ; 
-                    }
+                    ans[i] = 0 ; 
                 }
-            }
-            else{
-                ans.push_back(prod / nums[i] ) ; 
             }
         }
         return ans ;
+
     }
 };
