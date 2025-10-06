03ab12fef9b550c224c84106b1938c715cc52d78
Mayur <147459454+Mayursoni1518@users.noreply.github.com>
2025-10-06 09:17:07 +0530

    Time: 3 ms (66.1%), Space: 10.3 MB (60.38%) - LeetHub

--- 3502-count-substrings-with-k-frequency-characters-i/3502-count-substrings-with-k-frequency-characters-i.cpp
+++ 3502-count-substrings-with-k-frequency-characters-i/3502-count-substrings-with-k-frequency-characters-i.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     int numberOfSubstrings(string s, int k) {
-        int n = s.size();
-        int i = 0, j = 0, ans = 0;
-        unordered_map<char, int> mp;
-
-        while (j < n) {
-            mp[s[j]]++;
-
-            // Check if condition met
-            while (true) {
-                bool ok = false;
-                for (auto &p : mp) {
-                    if (p.second >= k) { 
-                        ok = true; 
-                        break;
+        int n = s.size() ; 
+        int i = 0 ;
+        int j = 0 ;
+        int ans = 0 ; 
+        unordered_map<char,int>mp ;
+        while(j < n ) {
+            mp[s[j]]++ ;
+            while(true) {
+                bool ok = false ; 
+                for(auto& i : mp ) {
+                    if(i.second >= k ) {
+                        ok = true ;
+                        break ; 
                     }
                 }
-                if (!ok) break;
-
-                // If valid, add all substrings starting at i and ending from j to n-1
-                ans += (n - j);
-                mp[s[i]]--;
-                i++;
+                if(!ok ) {
+                    break ; 
+                }
+                ans += n - j ; 
+                mp[s[i]]-- ;
+                i++ ; 
             }
-            j++;
+            j++ ; 
         }
-        return ans;
+        return ans ;
     }
 };
