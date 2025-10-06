054d94a1627e7742f8b4edd4c83b13357df71c19
Mayur <147459454+Mayursoni1518@users.noreply.github.com>
2025-10-06 06:12:30 +0530

    Time: 0 ms (100%), Space: 27.2 MB (80.34%) - LeetHub

--- 3381-shortest-subarray-with-or-at-least-k-i/3381-shortest-subarray-with-or-at-least-k-i.cpp
+++ 3381-shortest-subarray-with-or-at-least-k-i/3381-shortest-subarray-with-or-at-least-k-i.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     int minimumSubarrayLength(vector<int>& nums, int k) {
-        int i = 0, j = 0;
-        int curr_or = 0;  
-        int mini = INT_MAX;
-        int n = nums.size();  
-
+        int n = nums.size();
+        int curr_or = 0;
+        int i = 0;
+        int mini = INT_MAX; 
+        int j = 0;
         while (j < n) {
-            curr_or |= nums[j];  // Expand the window
+            curr_or |= nums[j];
+            while (curr_or >= k && i <= j) {
+                mini = min(mini, j -i + 1 );
 
-            while (curr_or >= k && i <= j) {  // Shrink the window when OR is >= k
-                mini = min(mini, j - i + 1);
-
-                // Recompute the OR for the new window
-                curr_or = 0;
-                for (int x = i + 1; x <= j && x < n; x++) {  // Ensure x does not exceed bounds
-                    curr_or |= nums[x];
+                curr_or = 0 ;
+                for(int x = i + 1 ; x <= j && x < n ; x++ ) {
+                    curr_or |= nums[x] ; 
                 }
-
-                i++;  
+                i++ ; 
             }
-
-            j++; 
+            j++ ; 
         }
-
-        return mini == INT_MAX ? -1 : mini;  
+        return mini == INT_MAX ? -1 : mini ; 
     }
 };
